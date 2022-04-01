ae059e02f24b447c78be7b9a0ea7fd9477b7a959
shaolang <1130429224@qq.com>
2022-04-01 10:14:01 +0800

    fix main

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 		m := make([]map[string]interface{}, 0)
 		db.Table("bg_user").Where("user_id= ?", "admin").Find(&m)
 		fmt.Println("user:", m)
+		fmt.Println("我是admin")
 		defer db.Close()
 		return c.HTML(http.StatusOK, pgConnString+"Hello, Docker! <3")
 	})
